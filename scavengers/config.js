05580889ec543dbe790124fdc598f42e93d5b3d6
NathanD88 <32457120+NathanD88@users.noreply.github.com>
2022-06-06 16:24:52 -0400

    Update config.js
    added consts

--- scavengers/config.js
+++ scavengers/config.js
@@ -1,1 +1,83 @@
+const outerwalls = [
+    "./resources/tiles/outerwall/outerwall1.png",
+    "./resources/tiles/outerwall/outerwall2.png",
+    "./resources/tiles/outerwall/outerwall3.png"
+]
 
+const floors = [
+    "./resources/tiles/floor/floor1.png",
+    "./resources/tiles/floor/floor2.png",
+    "./resources/tiles/floor/floor3.png",
+    "./resources/tiles/floor/floor4.png",
+    "./resources/tiles/floor/floor5.png",
+    "./resources/tiles/floor/floor6.png",
+    "./resources/tiles/floor/floor7.png",
+    "./resources/tiles/floor/floor8.png"
+]
+
+const walls = [
+    "./resources/tiles/wall/wall1.png",
+    "./resources/tiles/wall/wall2.png",
+    "./resources/tiles/wall/wall3.png",
+    "./resources/tiles/wall/wall4.png",
+    "./resources/tiles/wall/wall5.png",
+    "./resources/tiles/wall/wall6.png",
+    "./resources/tiles/wall/wall7.png",
+    "./resources/tiles/wall/wall8.png"
+]
+
+const damagedWalls = [
+    "./resources/tiles/wall/damaged/wall1.png",
+    "./resources/tiles/wall/damaged/wall2.png",
+    "./resources/tiles/wall/damaged/wall3.png",
+    "./resources/tiles/wall/damaged/wall4.png",
+    "./resources/tiles/wall/damaged/wall5.png",
+    "./resources/tiles/wall/damaged/wall6.png",
+    "./resources/tiles/wall/damaged/wall7.png",
+    "./resources/tiles/wall/damaged/wall8.png"
+]
+
+const playerIdle = [
+    "./resources/animated/player/idle/idle01.png",
+    "./resources/animated/player/idle/idle02.png",
+    "./resources/animated/player/idle/idle03.png",
+    "./resources/animated/player/idle/idle04.png",
+    "./resources/animated/player/idle/idle05.png",
+    "./resources/animated/player/idle/idle06.png"
+]
+const playerAttack = [
+    "./resources/animated/player/attack/attack01.png",
+    "./resources/animated/player/attack/attack02.png"
+]
+const playerHit = [
+    "./resources/animated/player/hit/hit01.png",
+    "./resources/animated/player/hit/hit02.png"
+]
+const zombie1Idle = [
+    "./resources/animated/zombie01/idle/idle01.png",
+    "./resources/animated/zombie01/idle/idle02.png",
+    "./resources/animated/zombie01/idle/idle03.png",
+    "./resources/animated/zombie01/idle/idle04.png",
+    "./resources/animated/zombie01/idle/idle05.png",
+    "./resources/animated/zombie01/idle/idle06.png"
+]
+const zombie1Attack = [
+    "./resources/animated/zombie01/attack/attack01.png",
+    "./resources/animated/zombie01/attack/attack02.png"
+]
+const zombie2Idle = [
+    "./resources/animated/zombie02/idle/idle01.png",
+    "./resources/animated/zombie02/idle/idle02.png",
+    "./resources/animated/zombie02/idle/idle03.png",
+    "./resources/animated/zombie02/idle/idle04.png",
+    "./resources/animated/zombie02/idle/idle05.png",
+    "./resources/animated/zombie02/idle/idle06.png"
+]
+const zombie2Attack = [
+    "./resources/animated/zombie02/attack/attack01.png",
+    "./resources/animated/zombie02/attack/attack02.png"
+]
+
+const exitSign = "./resources/tiles/exit.png";
+const FOOD = "./resources/tiles/food.png";
+const SODA = "./resources/tiles/soda.png";
